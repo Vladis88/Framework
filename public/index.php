496b336535b678616b6b6be58c4982c95e7de044
vladis <vladik.lajkov.01@bk.ru>
2021-04-19 23:38:39 +0300

    Modify index with zendframework/zend-stratigility...

--- public/index.php
+++ public/index.php
@@ -35,12 +35,7 @@ $aura = new RouterContainer();
 $routes = $aura->getMap();
 
 $routes->get('home', '/', HomeAction::class);
-
-$routes->get('cabinet', '/cabinet', [
-    new BasicAuthMiddleware($params['users']),
-    CabinetAction::class,
-]);
-
+$routes->get('cabinet', '/cabinet', CabinetAction::class);
 $routes->get('about', '/about', AboutAction::class);
 $routes->get('blog', '/blog', BlogAction::class);
 $routes->get('blog_show', '/blog/{id}', ShowAction::class)->tokens(['id' => '\d+']);
@@ -53,6 +48,7 @@ $app = new Application($resolver, new NotFoundHandler(), new Response());
 $app->pipe(new ErrorHandlerMiddleware($params['debug']));
 $app->pipe(CredentialsMiddleware::class);
 $app->pipe(ProfilerMiddleware::class);
+$app->pipe('/cabinet', new BasicAuthMiddleware($params['users']));
 $app->pipe(new RouteMiddleware($router));
 $app->pipe(new DispatchMiddleware($resolver));
 
